225fe5a9e0c99fa316c018d0056e59ea0884921a
Sharee Stark <shareegstark@gmail.com>
2024-02-23 10:51:03 -0500

    first branch

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function () { 
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else { cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+}
+});
